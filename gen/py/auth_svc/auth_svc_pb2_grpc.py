75bb8e99fd6397889bfe4da0cc6f9066aa84237a
SsSJKK <kobil07@mail.ru>
2024-04-11 15:00:43 +0500

    -f

--- gen/py/auth_svc/auth_svc_pb2_grpc.py
+++ gen/py/auth_svc/auth_svc_pb2_grpc.py
@@ -124,7 +124,8 @@ class AuthServicer(object):
         raise NotImplementedError('Method not implemented!')
 
     def ChangePassword(self, request, context):
-        """Missing associated documentation comment in .proto file."""
+        """rpc CheckVersion (CheckVersionRequest) returns (CheckVersionResponse);
+        """
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
